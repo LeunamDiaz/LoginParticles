bc1de288eddec77f5fac692c9669489f37e8b55c
marshallog <leunamdiazmx@gmail.com>
2024-05-29 12:13:04 -0600

    Login with stars moving

---
+++ script.js
@@ -0,0 +1,6 @@
+function Login() {
+  U = document.getElementById("U").value;
+  p = document.getElementById("P").value;
+
+  if (U == "Juan" && p == "123") window.location = "https://google.com";
+}
